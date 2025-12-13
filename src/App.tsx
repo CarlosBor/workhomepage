865cc75246591652a5f45e4087e0aa3065041c56
Carlos <carlosborjawebdev@gmail.com>
2025-12-13 01:28:37 +0100

    Update links, vite config for builds

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,15 @@ type LinkItem = {
 }
 
 const jobBoards: LinkItem[] = [
-  { name: 'LinkedIn', url: 'https://www.linkedin.com', icon: '💼' },
-  { name: 'InfoJobs', url: 'https://www.infojobs.net', icon: '📋' },
-  { name: 'Tecnoempleo', url: 'https://www.tecnoempleo.com', icon: '🧠' },
-  { name: 'Domestika', url: 'https://www.domestika.org', icon: '🎨' },
-  { name: 'Jooble', url: 'https://es.jooble.org', icon: '🦘' },
-  { name: 'Indeed', url: 'https://www.indeed.com', icon: '🔍' },
+  { name: 'LinkedIn', url: 'https://www.linkedin.com/jobs/search/?currentJobId=4343084923&f_TPR=r604800&f_WT=3%2C1&geoId=101962740&keywords=react&origin=JOB_SEARCH_PAGE_JOB_FILTER', icon: '💼' },
+  { name: 'InfoJobs', url: 'https://www.infojobs.net/jobsearch/search-results/list.xhtml?keyword=react&normalizedJobTitleIds=2512_866c7813-2c03-47d7-9bdc-192cfbace57c&provinceIds=51&cityIds=Bilbao&cityId=6693&searchByType=city&segmentId=&page=1&sortBy=RELEVANCE&onlyForeignCountry=false&countryIds=17&sinceDate=_7_DAYS', icon: '📋' },
+  { name: 'Tecnoempleo', url: 'https://www.tecnoempleo.com/ofertas-trabajo/?te=react&pr=241', icon: '🧠' },
+  { name: 'Domestika', url: 'https://www.domestika.org/en/search/jobs?query=react', icon: '🎨' },
+  { name: 'Jooble', url: 'https://es.jooble.org/SearchResult?date=2&rgns=Bilbao%2C%20Vizcaya&ukw=react', icon: '🦘' },
+  { name: 'Indeed', url: 'https://es.indeed.com/jobs?q=react&l=bilbao%2C+vizcaya+provincia', icon: '🔍' },
 ]
 
 const consultancies: LinkItem[] = [
-  { name: 'Ibermática', url: 'https://www.ibermatica.com', icon: '🏢' },
-  { name: 'Kyndryl', url: 'https://www.kyndryl.com', icon: '🧠' },
-  { name: 'Accenture', url: 'https://www.accenture.com', icon: '🚀' },
   { name: 'Sopra Steria', url: 'https://www.soprasteria.es', icon: '🧭' },
   { name: 'Inetum', url: 'https://www.inetum.com', icon: '🌐' },
   { name: 'Sener', url: 'https://www.group.sener', icon: '⚙️' },
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/workhomepage/',
+  base: '/workhomepage/',
+  build: {
+    outDir: 'docs',           // change 'dist' → 'public' or any folder
+  },
 })
